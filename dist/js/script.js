015ef5578f159b30d4cab331c041f6b80c8bf127
BillyJonathan29 <billyjonathan048@gmail.com>
2025-03-07 11:37:57 +0700

    on progres dark toggle on going to dark mode

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,6 +2,7 @@
 
 const hamburger = document.querySelector('#hamburger');
 const navMenu = document.querySelector('#nav-menu');
+const toTop = document.querySelector('#to-top')
 
 hamburger.addEventListener('click', function(){
     hamburger.classList.toggle('hamburger-active');
@@ -10,14 +11,27 @@ hamburger.addEventListener('click', function(){
 
 
 // navbar fixed 
-
 window.onscroll = function() { 
     const header = document.querySelector('header');
     const fixedNav = header.offsetTop;
 
     if (window.pageYOffset > fixedNav) {
         header.classList.add('navbar-fixed');
+        toTop.classList.remove('hidden');
+        toTop.classList.add('flex')
     } else{
         header.classList.remove('navbar-fixed');
+        toTop.classList.remove('flex');
+        toTop.classList.add('hidden')
     }
 }
+
+
+
+// klik di luar hamburger
+window.addEventListener('click', function(e){
+  if(e.target != hamburger && e.target != navMenu){
+    hamburger.classList.remove('hamburger-active');
+    navMenu.classList.add('hidden');
+  }  
+})
