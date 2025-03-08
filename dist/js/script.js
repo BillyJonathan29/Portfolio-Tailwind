8609d22b6db30f0a359818f2ad057bfaf772561a
BillyJonathan29 <billyjonathan048@gmail.com>
2025-03-08 12:42:52 +0700

    add image for project

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,37 +1,60 @@
 // hamburger
 
-const hamburger = document.querySelector('#hamburger');
-const navMenu = document.querySelector('#nav-menu');
-const toTop = document.querySelector('#to-top')
+const hamburger = document.querySelector("#hamburger");
+const navMenu = document.querySelector("#nav-menu");
+const toTop = document.querySelector("#to-top");
 
-hamburger.addEventListener('click', function(){
-    hamburger.classList.toggle('hamburger-active');
-    navMenu.classList.toggle('hidden')
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("hamburger-active");
+  navMenu.classList.toggle("hidden");
 });
 
+// navbar fixed
+window.onscroll = function () {
+  const header = document.querySelector("header");
+  const fixedNav = header.offsetTop;
 
-// navbar fixed 
-window.onscroll = function() { 
-    const header = document.querySelector('header');
-    const fixedNav = header.offsetTop;
-
-    if (window.pageYOffset > fixedNav) {
-        header.classList.add('navbar-fixed');
-        toTop.classList.remove('hidden');
-        toTop.classList.add('flex')
-    } else{
-        header.classList.remove('navbar-fixed');
-        toTop.classList.remove('flex');
-        toTop.classList.add('hidden')
-    }
-}
-
-
+  if (window.pageYOffset > fixedNav) {
+    header.classList.add("navbar-fixed");
+    toTop.classList.remove("hidden");
+    toTop.classList.add("flex");
+  } else {
+    header.classList.remove("navbar-fixed");
+    toTop.classList.remove("flex");
+    toTop.classList.add("hidden");
+  }
+};
 
 // klik di luar hamburger
-window.addEventListener('click', function(e){
-  if(e.target != hamburger && e.target != navMenu){
-    hamburger.classList.remove('hamburger-active');
-    navMenu.classList.add('hidden');
-  }  
-})
+window.addEventListener("click", function (e) {
+  if (e.target != hamburger && e.target != navMenu) {
+    hamburger.classList.remove("hamburger-active");
+    navMenu.classList.add("hidden");
+  }
+});
+
+// dark toggle
+document.addEventListener("click", function () {
+  const toggle = document.getElementById("dark-toggle");
+  const toggleCircle = document.querySelector(".toggle-circle");
+
+  toggle.addEventListener("change", function () {
+    if (this.checked) {
+      toggleCircle.style.transform = "translateX(16px)";
+    } else {
+      toggleCircle.style.transform = "translateX(0px)";
+    }
+  });
+});
+
+// // dark mode
+const darkToggle = document.querySelector("#dark-toggle");
+const html = document.querySelector("html");
+
+darkToggle.addEventListener("click", function () {
+  if (darkToggle.checked) {
+    html.classList.add("dark");
+  } else {
+    html.classList.remove("dark");
+  }
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
-  
+  darkMode: "class",
+
   theme: {
     container: {
       center: true,
-      padding: '16px'
+      padding: "16px",
     },
     extend: {
       colors: {
-        secondary: '#64748b',
-        primary: '#3b82f6',
-        dark: '#020617'
+        secondary: "#64748b",
+        primary: "#3b82f6",
+        // dark: "#020617",
       },
       screens: {
-        '2xl': '1320px',
+        "2xl": "1320px",
       },
     },
   },
   plugins: [],
-}
+};
